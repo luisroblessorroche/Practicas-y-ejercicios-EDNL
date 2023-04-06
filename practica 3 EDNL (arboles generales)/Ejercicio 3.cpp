b85b1da9c913cfef71daf4b205f2fb3cb809d515
luisroblessorroche <luis.roblessorroche@alum.uca.es>
2023-04-06 17:26:23 +0200

    a

--- practica 3 EDNL (arboles generales)/Ejercicio 3.cpp
+++ practica 3 EDNL (arboles generales)/Ejercicio 3.cpp
@@ -11,7 +11,30 @@ template <typename T> int desequilibrio_agen(const Agen<T> &A)
 	return desequilibrio_agen_rec(A.raiz(),A);
 }
 
+
 template <typename T> int desequilibrio_agen_rec(typename Agen<T>::nodo n, const Agen<T> &A)
+{
+	if(n == Agen<T>::NODO_NULO)
+	{
+		return 0;
+	}
+	else
+	{
+		Agen<T>::nodo hijo = A.hijoIzqdo(n);
+		
+		while(hijo != Agen<T>::NODO_NULO)
+		{
+			int altura_max = altura_max(n,A);
+			int altura_min = altura_min(n,A);
+			
+			int desequilibrio = max(altura_max-altura_min, desequilibrio_agen_rec(A.hijoIzqdo(n),A));
+			hijo = A.hermDrcho(n);
+		}
+		return desequilibrio;
+	}
+}
+
+template <typename T> int altura_max(typename Agen<T>::nodo n, const Agen<T> &A)
 {
 	if(n == Agen<T>::NODO_NULO)
 	{
@@ -19,42 +42,20 @@ template <typename T> int desequilibrio_agen_rec(typename Agen<T>::nodo n, const
 	}
 	else
 	{
-		Agen<T>::nodo hijo = A.hijoIzqdo(n);
-		int altura_min = altura_nodo(hijo,A);
-		int altura_max = altura_min;
-		
-		hijo = A.hermDrcho(hijo);
-		
-		while(hijo != Agen<T>::NODO_NULO)
-		{
-			int altura = altura_nodo(hijo,A);
-			if(altura > altura_max)
-			{
-				altura_max = altura;
-			}
-			if(altura < altura_min)
-			{
-				altura_min = altura;
-			}
-			
-			hijo = A.hermDrcho(hijo);
-		}
-		
-		int dif = altura_max - altura_min;
-		
-		Agen<T>::nodo m = A.hijoIzqdo(n);
-		
-		while(m != Agen<T>::NODO_NULO)
-		{
-			return 
-		}
+		return 1 + max(altura_max(A.hijoIzqdo(n),A),altura_max(A.hermDrcho(n),A));
 	}
 }
 
-
-template <typename T> int desequilibrio_agen_rec(typename Agen<T>::nodo n, const Agen<T> &A)
+template <typename T> int altura_min(typename Agen<T>::nodo n, const Agen<T> &A)
 {
-	
+	if(n == Agen<T>::NODO_NULO)
+	{
+		return -1;
+	}
+	else
+	{
+		return 1 + min(altura_min(A.hijoIzqdo(n),A),altura_min(A.hermDrcho(n),A));
+	}
 }
 
 
